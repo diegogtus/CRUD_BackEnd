56657fac5c8ee3d242daa92013e7eeae708f4918
root <root@ip-172-31-7-207.us-east-2.compute.internal>
2019-12-04 06:10:50 +0000

    Final version. Navbar bug, change the IP adress.

--- Node/server.js
+++ Node/server.js
@@ -9,7 +9,13 @@ const cors = require('cors');
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 //Database Connection
 MongoClient.connect(db.url, (err, database) => {  if (err) return console.log(err)                        // Make sure you add the database name and not the collection name  
     const Database = database.db("CRUD")  
@@ -22,10 +28,10 @@ MongoClient.connect(db.url, (err, database) => {  if (err) return console.log(er
 //headers para CORS
 var originsWhitelist = [
   //'http://localhost:4200'
-   'http://13.58.204.173:4200'
+   'http://3.135.212.39:8000:4200'
 ];
 var corsOptions = {
-origin: function(origin, callback){
+ origin: function(origin, callback){
       var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
       callback(null, isWhitelisted);
 },
